b690f4e37fbd931c3cf06891eaa97808f630f976
Bianca <bianca.ionashiro@gmail.com>
2026-01-12 21:51:45 -0300

    Ajustes

---
+++ Tests/SistemaVendas.TestesIntegracao/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace SistemaVendas.TestesIntegracao
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Tests/SistemaVendas.TestesUnidade/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace SistemaVendas.TestesUnidade
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
