cf9100316634c12a814ed29d78c79df4ecd593d5
Bianca <bianca.ionashiro@gmail.com>
2026-01-19 14:11:28 -0300

    Instalação de pacotes Dapper e Framework.

---
+++ SistemaVendas.Dominio/Entidades/Geral/GrupoUnidadeMedida.cs
@@ -0,0 +1,8 @@
+﻿namespace SistemaVendas.Dominio.Entidades.Geral;
+
+public sealed class GrupoUnidadeMedida(int VOL, string volume, UnidadeMedida unidadeMedidaPadrao)
+{
+    public int Codigo { get; } = VOL;
+    public string Descricao { get; set;  } = volume;
+    public UnidadeMedida UnidadeMedidaPadrao { get;} = unidadeMedidaPadrao;
+}
---
+++ SistemaVendas.Dominio/Entidades/Geral/UnidadeMedida.cs
@@ -0,0 +1,8 @@
+﻿namespace SistemaVendas.Dominio.Entidades.Geral;
+
+public sealed class UnidadeMedida(int codigo, string descricao, GrupoUnidadeMedida grupo)
+{
+    public int Codigo { get; } = codigo;
+    public string Descricao { get;} = descricao;
+    public GrupoUnidadeMedida Grupo { get; } = grupo;
+}
